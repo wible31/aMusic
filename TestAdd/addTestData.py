a908e1616ad482cd6588f644dbb2cf19a7409786
ilins <ilinspavel07@hmail.com>
2025-01-10 21:06:26 +0300

    servet_test fix

---
+++ TestAdd/addTestData.py
@@ -0,0 +1,18 @@
+import datetime
+import json
+
+from data import db_session
+from data.tests import Test
+from data.users import User
+
+
+def add_test_users(db_sess):
+    user = User()
+    user.name = 'Jerainee'
+    user.about = 'ШедевроОписание'
+    user.email = 'ilinspavel07@gmail.com'
+    user.password = '11111'
+    user.created_date = datetime.datetime.now()
+
+    db_sess.add(user)
+#  db_sess.commit()
